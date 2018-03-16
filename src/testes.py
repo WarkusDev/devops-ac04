075e1d99fb6a5a6571b18287f4711bb320eebd5f
WarkusDev <36314569+WarkusDev@users.noreply.github.com>
2018-03-16 20:24:50 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -8,10 +8,10 @@ else:
 	for linha in jogo:
 		if len(linha) != 3:
 			erroInicializar = True
-else:
-	for elemento in linha:
-		if elemento != '.':
-			erroInicializar = True
+		else:
+			for elemento in linha:
+				if elemento != '.':
+					erroInicializar = True
 if erroInicializar:
 	sys.exit(1)
 else:
